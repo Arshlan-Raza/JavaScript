531907db0e3e92ce13a38e55691386c3c18c5daa
Arshlan <arshalanraja@gmail.com>
2023-10-18 10:28:13 +0530

    #

--- JS Basics 2/index.js
+++ JS Basics 2/index.js
@@ -14,3 +14,19 @@ const rectangle = {
 
     }
 };
+
+
+function createSquare(side) {
+
+    return square =  {
+
+        S:side,
+
+        draw( ){
+            console.log('The side of the square  is :'+S);
+        }
+
+    };
+}
+
+let squareObj1 = createSquare(5);
