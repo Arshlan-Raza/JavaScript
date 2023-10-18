8ad43f6209a0acd51f3eda4e32104813da4d60db
Arshlan <arshalanraja@gmail.com>
2023-10-18 17:10:43 +0530

    #

--- JS Basics 3/index.js
+++ JS Basics 3/index.js
@@ -8,3 +8,18 @@ let numbers = [1,2,'arshlan','raza'];
 
 
 console.log(numbers);
+
+
+let course = [
+    {no:1 , naam:'Arshlan'},
+    {no:2,  naam:'Raza'}
+];
+
+
+let cour = course.find(function(course){
+
+    return course.naam == 'Arshlan'
+})
+
+
+console.log(cour);
