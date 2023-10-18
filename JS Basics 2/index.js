a5eb36c508a3881e64e021cdfba2f27f0f7f8d4a
Arshlan <arshalanraja@gmail.com>
2023-10-18 09:59:39 +0530

    second day of learning js

---
+++ JS Basics 2/index.js
@@ -0,0 +1,16 @@
+
+
+console.log('Chaliye Shuru karte hai ');
+
+const rectangle = {
+
+    length:1 , 
+
+    breadth : 2,
+
+    draw: function(){
+
+        console.log('drawing rectangle');
+
+    }
+};
