995cc1234fa9f4a50911e1066c02b3afe88ab7cb
Arshlan <arshalanraja@gmail.com>
2023-10-22 17:18:13 +0530

    #

--- JS Basics 4/index.js
+++ JS Basics 4/index.js
@@ -30,8 +30,9 @@
 
 // console.log(ans);
 
-function sum (...args){
+function sum (num,value,...args){ // here num = 1, value = 2 ..args (this should be the last parameter)
     console.log(args);
 }
 
 sum(1,2,3,4,5,6);
+
