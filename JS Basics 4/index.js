403ce203a3204af5276872594211bec54ec48654
Arshlan <arshalanraja@gmail.com>
2023-10-22 17:30:21 +0530

    #

--- JS Basics 4/index.js
+++ JS Basics 4/index.js
@@ -43,6 +43,14 @@ let person = {
 
     get fullName(){
         return `${person.fName} ${person.lName}`
+    },
+
+    set fullName(value){
+        let parts = value.split('');
+        this.fName = parts[0];
+        this.lName = parts[1];
     }
     
-}
+};
+
+console.log(person.fullName);
