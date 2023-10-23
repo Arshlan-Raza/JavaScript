9eb63aa1e1c0f4958dc83478de642fa5a75d5cd0
Arshlan <arshalanraja@gmail.com>
2023-10-23 15:49:59 +0530

    #

--- JSDOM2/index.js
+++ JSDOM2/index.js
@@ -14,20 +14,22 @@
 
 // })
 
+let myDiv = document.createElement('div');
+
 function paraStatus(event){
 
-    console.log("I have clicked on para ");
+    console.log("Para "+event.target.textContent);
 
 }
 
-let myDiv = document.createElement('div');
+myDiv.addEventListener('click' , paraStatus)
 
 for(let i = 1 ; i<= 100 ; i++){
 
         let newElement = document.createElement('p');
         newElement.textContent = "This is para "+i;
 
-        newElement.addEventListener('click' , paraStatus)
+      
         myDiv.appendChild(newElement); 
 }
 
