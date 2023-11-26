3bc21e1d80a8b0f7ac9658315ab49461d936855f
Arshlan <arshalanraja@gmail.com>
2023-11-26 10:32:22 +0530

    continuing the revision

--- JS_Revise/file.js
+++ JS_Revise/file.js
@@ -14,41 +14,70 @@
 
 // console.log(answ)
 
-// factory function 
+                // factory function 
 
-function createRectangle (){
-    let rectangle = {
+// function createRectangle (){
+//     let rectangle = {
 
-        length : 1 , 
+//         length : 1 , 
 
-        breadth : 2 ,
+//         breadth : 2 ,
 
-        draw : function(){
-            console.log('drawing rectangle')
-        }
-    };
+//         draw : function(){
+//             console.log('drawing rectangle')
+//         }
+//     };
 
-    return rectangle
+//     return rectangle
 
-}
+// }
 
-let rectangle2 = createRectangle()
+// let rectangle2 = createRectangle()
 
-console.log(rectangle2.length)
+// console.log(rectangle2.length)
 
-function Rectangle() { 
+// function Rectangle() { 
 
-    this.length = 1 ; 
+//     this.length = 1 ; 
 
-    this.breadth = 3 ; 
+//     this.breadth = 3 ; 
 
-    this.draw = function(){
+//     this.draw = function(){
 
-        console.log('drawing')
+//         console.log('drawing')
 
-    }
-}
+//     }
+// }
 
-let rectangle1 = new Rectangle()
+// let rectangle1 = new Rectangle()
 
-console.log(rectangle1.draw())
+// console.log(rectangle1.draw())
+
+
+// let rectangle = {
+
+//     length : 2,
+//     breadth : 4
+
+// }
+// if('length' in rectangle){
+//     console.log('is present')
+// }
+            // revising the concepts of cloning 
+                // 1.Iteration 
+// let src = {value : 10,assign : 20}
+
+// let dest = {}
+
+// for (let key in src){
+//     dest[key] = src [key]
+// }
+
+// console.log(dest)
+
+                // 2.Assign 
+
+let src = {value : 10,assign : 20}
+
+let dest = object.assign({},src)
+
