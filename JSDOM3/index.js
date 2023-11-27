09b03053aaaed3a4942cfca463a085c39301718f
Arshlan <arshalanraja@gmail.com>
2023-11-27 15:35:59 +0530

    #

--- JSDOM3/index.js
+++ JSDOM3/index.js
@@ -1,34 +1,45 @@
 //  adding 100 para
 
-const t1 = performance.now();
+// const t1 = performance.now();
 
-for(let i = 1; i <= 100 ; i++){
+// for(let i = 1; i <= 100 ; i++){
 
-    let newElement = document.createElement('p');
-    newElement.textContent = 'This is para'+i;
+//     let newElement = document.createElement('p');
+//     newElement.textContent = 'This is para'+i;
 
-    document .body.appendChild(newElement);
-}
+//     document .body.appendChild(newElement);
+// }
 
-const t2 = performance.now();
-console.log('this took '+(t2-t1)+'ms');
+// const t2 = performance.now();
+// console.log('this took '+(t2-t1)+'ms');
 
 
-// optimizing a bit 
+// // optimizing a bit 
 
-const t3 = performance.now();
+// const t3 = performance.now();
 
-let myDiv = document.createElement('p');
+// let myDiv = document.createElement('p');
 
+// for(let i = 1 ; i <= 100 ; i++){
+
+//     let element = document.createElement('p');
+//     element.textContent = 'This is para ' + i ;
+
+//     myDiv.appendChild(element);
+// }
+
+// document.body.appendChild(myDiv);
+
+// const t4 = performance.now();
+// console.log('this took '+(t4-t3)+'ms');
+
+let fragment = document.createDocumentFragment();
 for(let i = 1 ; i <= 100 ; i++){
 
     let element = document.createElement('p');
     element.textContent = 'This is para ' + i ;
 
-    myDiv.appendChild(element);
+    fragment.appendChild(element);
 }
 
-document.body.appendChild(myDiv);
-
-const t4 = performance.now();
-console.log('this took '+(t4-t3)+'ms');
+document.body.appendChild(fragment); // 1 reflow and 1 repaint (performance is increased )
