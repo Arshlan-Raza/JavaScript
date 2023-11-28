e0726f025294c7be048cf38920898cd70be9c406
Arshlan <arshalanraja@gmail.com>
2023-11-28 22:03:40 +0530

    async and await

--- JSDOM4/index.js
+++ JSDOM4/index.js
@@ -29,11 +29,56 @@
 
 // console.log('Pehla');
 
-let meraPromise = new Promise (function(resolve, reject){
-    setTimeout(function(){
-       console.log('I am inside Promise');
-    },5000);
-    reject(new Error('BhaiSahab error aa gya'))
-});
+// let meraPromise = new Promise (function(resolve, reject){
+//     setTimeout(function(){
+//        console.log('I am inside Promise');
+//     },5000);
+//     reject(new Error('BhaiSahab error aa gya'))
+// });
 
-meraPromise.catch((error) => {console.log('Recieved an error')});
+// meraPromise.catch((error) => {console.log('Recieved an error')});
+
+
+// let wadda1 = new Promise(function(resolve,  reject){
+//     setTimeout(() => {
+//         console.log('setTimeout1 started');
+//     }, 2000);
+//     resolve(true);
+// })
+
+// let output = wadda1.then(() =>{
+//     let wada2 = new Promise(function(resolve,reject){
+//         setTimeout(() => {
+//             console.log('setTimeout2 started');
+//         }, 2000);
+//         resolve('wada 2 resolved');      
+//     })
+//     return wada2;
+// })
+
+// output.then((value) => console.log(value));
+
+// async function abcd(){
+//     return 'Arshlan';
+// }
+
+// console.log(abcd());
+async function utility(){
+
+    let jharkhandMausam = new Promise ((resolve,reject) => {
+        setTimeout(() => {
+            resolve('Jharkhand me bahut thand hai')
+        }, 1000);
+    });
+    
+    let delhiMausam = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Delhi me bahut grmi hai')
+        }, 5000);
+    });
+    
+    let jM = await jharkhandMausam;
+    let dM = await delhiMausam;
+
+    return [jM,dM];
+}
