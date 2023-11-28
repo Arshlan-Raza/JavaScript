0e3692322374bd68d29a6573872f94143df74ac5
Arshlan <arshalanraja@gmail.com>
2023-11-28 21:21:19 +0530

    learnt about promise

--- JSDOM4/index.js
+++ JSDOM4/index.js
@@ -5,16 +5,35 @@
 // console.log('second');
 
 
-setTimeout(function(){
-    console.log('third');
-},3000)
+// setTimeout(function(){
+//     console.log('third');
+// },3000)
 
 
-function sync() { 
+// function sync() { 
 
-    console.log('first');
-}
+//     console.log('first');
+// }
 
-sync();
+// sync();
 
-console.log('second')
+// console.log('second')
+
+
+// let meraPromise = new Promise (function(resolve, reject){
+//      setTimeout(function(){
+//         console.log('I am inside Promise');
+//      },5000);
+//      reject(new  Error('BhaiSahab error aa gya'))
+// });
+
+// console.log('Pehla');
+
+let meraPromise = new Promise (function(resolve, reject){
+    setTimeout(function(){
+       console.log('I am inside Promise');
+    },5000);
+    reject(new Error('BhaiSahab error aa gya'))
+});
+
+meraPromise.catch((error) => {console.log('Recieved an error')});
