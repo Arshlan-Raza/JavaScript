2c69f748376c25a78fa8907e10e58ce3747fb3e7
Arshlan <arshalanraja@gmail.com>
2023-12-11 19:42:39 +0530

    revising js

---
+++ JS by DB/dataTypes.js
@@ -0,0 +1,20 @@
+// primitive dataTypes 
+
+
+// Numbers
+const day = 23;
+
+// Strings
+const name = "Arshlan";
+
+// boolean
+const isTrue = true;
+
+// undefined
+let month;  // this is undefined only if it is declared using undefined 
+
+// null 
+let value = null;
+
+// symbol
+const sym = symbol();
