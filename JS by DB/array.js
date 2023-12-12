887fb121540cdf24cc38d35fa7819833a9f9bfee
Arshlan <arshalanraja@gmail.com>
2023-12-12 21:49:05 +0530

    revising array

---
+++ JS by DB/array.js
@@ -0,0 +1,12 @@
+const listUsers = ['Arshlan','Raza','Kaif'];
+
+console.log(typeof listUsers);
+
+// listUsers.pop();
+// listUsers.push();
+// listUsers.shift();
+// listUsers.unshift("New"); 
+
+
+
+console.log(listUsers.indexOf('Raza'));
