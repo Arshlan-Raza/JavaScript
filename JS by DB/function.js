efd0afe4e5cd9d871b333ef029d006f15e788379
Arshlan <arshalanraja@gmail.com>
2023-12-12 21:05:27 +0530

    revised the function concept

---
+++ JS by DB/function.js
@@ -0,0 +1,15 @@
+// function greet(name){
+//     console.log(`Welcome to our website ${name}`);
+// }
+
+// function signUp(){
+//     let name = prompt("What is your name?");
+//     greet(name);
+// }
+
+// signUp();
+
+
+let maxNo = Math.max(1,234,5,3,9);
+
+console.log(maxNo);
