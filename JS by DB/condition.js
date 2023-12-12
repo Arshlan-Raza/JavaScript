10b61d6cbbfd4b3f8b138670fbf98b6c9842f037
Arshlan <arshalanraja@gmail.com>
2023-12-12 21:05:12 +0530

    learnt about the contdition

---
+++ JS by DB/condition.js
@@ -0,0 +1,28 @@
+
+// const access = false ;
+
+// if(access){
+//     console.log("You have the access\n");
+// }
+// else{
+//     console.log("You don't have the access\n");
+// }
+
+
+
+// const age = 15 ;
+
+// if(age >= 18){
+//     console.log("You have the access\n");
+// }
+// else{
+//     console.log("You don't have the access\n");
+// }
+
+const doc = document.querySelector(".btn");
+
+doc.addEventListener('click',() => {
+
+    alert("you clicked the button");
+    
+});
