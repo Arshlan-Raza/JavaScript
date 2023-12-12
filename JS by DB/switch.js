7ad64379f517c7626eb59d931e5a629211e2cd06
Arshlan <arshalanraja@gmail.com>
2023-12-12 21:49:28 +0530

    revising switch

---
+++ JS by DB/switch.js
@@ -0,0 +1,18 @@
+let userInput = prompt('Tell me a fruit');
+
+
+switch(userInput){
+    case 'Orange':
+        alert('Orange is best for energy');
+        break;
+    case  'Apple':
+        alert("A apple a day keeps the doctor away!");
+        break;
+    case 'Grapes':
+        alert('Grapes juices are good');
+        break;
+    default:
+        alert("Never heard of that fruit");
+        break;
+
+}
