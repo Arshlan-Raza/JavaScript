bf3d02fe051559e898b181ef5077f7d757b1aac9
Arshlan <arshalanraja@gmail.com>
2023-12-13 22:39:56 +0530

    revised the loop

---
+++ JS by DB/loops.js
@@ -0,0 +1,44 @@
+const text = ['a','b','c','d','e','f','g'];
+
+
+// for loop
+// for(let i = 0 ; i < text.length; i++){
+//     console.log(text[i]);
+// }
+
+//for-of loop 
+// for(let name of text){
+//     console.log(name);
+// }
+
+// for-each loop  it is specially for arrays
+// text.forEach(function(name,index){
+//     console.log(`${name} ${index}`);
+//     // if(name === 'c'){
+//     //     break;               break statement works only in loop as it is inside a function 
+//     // }
+// })
+
+// let user = {
+
+// //  key    value = (property i.e, name: 'Arshlan)
+//     name: 'Arshlan',
+//     age : 20,
+//     college: 'PES',
+//     dept: 'ise'
+
+// };
+
+// // accessing the object 
+// console.log(user['name']);
+
+// //or
+// console.log(user.name);
+
+
+// // for in loop works in objects 
+// for(let x in user){
+//     console.log(x); // it will print the name of key present inside
+//     console.log(user[x]);  // it will return the value of the object
+// }
+
