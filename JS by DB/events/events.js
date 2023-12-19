8bb651c30714beee4a654719c33ab5d8ccff85af
Arshlan <arshalanraja@gmail.com>
2023-12-19 07:11:56 +0530

    #

--- JS by DB/events/events.js
+++ JS by DB/events/events.js
@@ -1,12 +1,17 @@
-const todoList = document.querySelector("#todo-list b");
+const todoList = document.querySelector("#todo-list ");
 const items  = todoList.children;
 const button = document.querySelector(".btn");
-const todoNR = document.querySelector(".todo-nr");
+const todoNR = document.querySelector(".todo-nr b");
+const title = document.querySelector(".title-container");
+
+// button.addEventListener('click',function(){
+//     let newItem = document.createElement('li');
+//     newItem.classList.add('item');
+//     newItem.innerText = `Item ${items.length + 1}`;
+//     todoList.appendChild(newItem);
+//     todoNR.innerText = items.length;
+// });
 
 button.addEventListener('click',function(){
-    let newItem = document.createElement('li');
-    newItem.classList.add('item');
-    newItem.innerText = `Item ${items.length + 1}`;
-    todoList.appendChild(newItem);
-    todoList.innerText = `items.length`;
-});
+    title.classList.toggle('spectacular')
+})
