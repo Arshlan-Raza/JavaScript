0bb30662242843099956f220c066a63d41f03131
Arshlan <arshalanraja@gmail.com>
2023-12-20 21:24:23 +0530

    event bubbling

--- JS by DB/events/events.js
+++ JS by DB/events/events.js
@@ -1,17 +1,27 @@
-const todoList = document.querySelector("#todo-list ");
-const items  = todoList.children;
+const todoList = document.querySelector("#todo-list");
+const items = todoList.children;
 const button = document.querySelector(".btn");
 const todoNR = document.querySelector(".todo-nr b");
 const title = document.querySelector(".title-container");
+const removeBtn = document.querySelector(".rmve");
+const nameInput = document.querySelector(".name-input")
 
-// button.addEventListener('click',function(){
-//     let newItem = document.createElement('li');
-//     newItem.classList.add('item');
-//     newItem.innerText = `Item ${items.length + 1}`;
-//     todoList.appendChild(newItem);
-//     todoNR.innerText = items.length;
-// });
+button.addEventListener('click', function (e) {
+    e.preventDefault();
+    let newItem = document.createElement('li');
+    newItem.classList.add('item');
+    newItem.innerText = nameInput.value;
+    nameInput.value = "";
+    todoList.appendChild(newItem);
+    todoNR.innerText = items.length;
+});
 
-button.addEventListener('click',function(){
-    title.classList.toggle('spectacular')
-})
+removeBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+   const  lastItem = todoList.lastElementChild;
+
+   if(lastItem){
+    lastItem.remove()
+    todoNR.innerHTML = items.length;
+   }
+});
