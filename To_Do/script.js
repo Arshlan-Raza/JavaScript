8d08d326404c72393f3e3be8993340d4dbb37472
Arshlan <arshalanraja@gmail.com>
2023-12-22 22:24:19 +0530

    adding the todo in local storage

--- To_Do/script.js
+++ To_Do/script.js
@@ -2,12 +2,13 @@
 const  todoInput = document.querySelector('.todo-input');
 const  todoButton = document.querySelector('.todo-button');  // to duplicate  hold shift+alt+down_arrow
 const  todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo')
 
 
 //event listeners
 todoButton.addEventListener('click',addTodo);
 todoList.addEventListener('click',deleteCheck);
-
+filterOption.addEventListener('click',filterTodo);
 
 
 //functions
@@ -59,3 +60,34 @@ function deleteCheck(e){
         todo.classList.toggle("completed");
 }
 
+function filterTodo(e){
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        switch(e.target.value){
+            case "all":
+                todo.style.display = 'flex';
+                break;
+            case "completed":
+                if(todo.classList.contains('completed')){
+                    todo.style.display='flex';
+                }
+                else{
+                    todo.style.display = 'none';
+                }
+                break;
+            case "uncompleted":
+                if(!todo.classList.contains('completed')){
+                    todo.style.display='flex';
+                }
+                else{
+                    todo.style.display = 'none';
+                }
+                break;
+        }
+    });
+}
+
+
+function saveLocalTodos(todo){
+    
+}
